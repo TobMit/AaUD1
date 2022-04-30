9b94129e1595a2235ec4dd5ad0a67882aa0fe0e2
TobMit <80719513+TobMit@users.noreply.github.com>
2022-04-30 23:40:12 +0200

    Test loadera - zle nacitava symboli treba poriesit

--- semestralnaPraca/main.cpp
+++ semestralnaPraca/main.cpp
@@ -6,7 +6,22 @@
 
 int main()
 {
-    std::cout << "Hello world again!" << std::endl;
-    DataLoader dataLoader;
+    DataLoader *loader = new DataLoader("../semestralnaPraca/dataLoader/rawData/kraje.csv");
+    if (loader->isOpen()) {
+        while (loader->nextLine()) {
+            wcout << loader->getNextParameter() << L" "
+                    << loader->getNextParameter() << L" "
+                    << loader->getNextParameter() << L" "
+                    << loader->getNextParameter() << L" "
+                    << loader->getNextParameter() << L" "
+                    << loader->getNextParameter() << L" "
+                    << loader->getNextParameter() << L" "
+                    << loader->getNextParameter() << L" "
+                    << loader->getNextParameter() << L" \n";
+        }
+    }
+
+
+    delete loader;
     return 0;
 }
