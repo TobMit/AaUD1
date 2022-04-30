9d2e714b655edef3e204691e5b0029fc418163a8
TobMit <80719513+tobmit@users.noreply.github.com>
2022-04-30 12:34:24 +0200

    Začiatok dataLoadera.

---
+++ SemestralnaPraca/dataLoader/dataLoader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tobias on 30/04/2022.
+//
+
+#include "dataLoader.h"
---
+++ SemestralnaPraca/dataLoader/dataLoader.h
@@ -0,0 +1,14 @@
+//
+// Created by Tobias on 30/04/2022.
+//
+#pragma once
+#ifndef AUS_CONSOLE_DATALOADER_H
+#define AUS_CONSOLE_DATALOADER_H
+
+
+class dataLoader {
+
+};
+
+
+#endif //AUS_CONSOLE_DATALOADER_H
