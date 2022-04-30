59887aaec2d27e2dcab88828b895cb80d02296cc
TobMit <80719513+TobMit@users.noreply.github.com>
2022-04-30 23:46:35 +0200

    Pridanie údajovych štruktúr ktoré budú uchovávať riadky v jednotlivých súboroch.

---
+++ semestralnaPraca/dataStructure/kraj.h
@@ -0,0 +1,11 @@
+//
+// Created by Tobias on 30. 4. 2022.
+//
+#pragma once
+
+
+#include "storedData.h"
+
+class Kraj : public StoredData {
+
+};
---
+++ semestralnaPraca/dataStructure/storedData.h
@@ -0,0 +1,12 @@
+//
+// Created by Tobias on 30. 4. 2022.
+//
+#pragma once
+
+///Virtualny trieda ktorú bude dedit kazda trieda ktora uchovava ulozene data
+class StoredData {
+public:
+    /// destruktor
+    virtual ~StoredData() = 0;
+
+};
