63e441544804d2add89b80923cd7e21f193553f0
TobMit <80719513+tobmit@users.noreply.github.com>
2022-05-02 01:08:39 +0200

    Nie uplne funkčné. linker robi blbosti

--- semestralnaPraca/main.cpp
+++ semestralnaPraca/main.cpp
@@ -3,9 +3,12 @@
 //
 #include "iostream"
 #include "dataLoader.h"
+#include "uzemnaJednotka.h"
+#include "storedData.h"
 
 int main()
 {
+    /*
     DataLoader *loader = new DataLoader("../semestralnaPraca/dataLoader/rawData/kraje.csv");
     if (loader->isOpen()) {
         while (loader->nextLine()) {
@@ -21,7 +24,27 @@ int main()
         }
     }
 
-
+    //cout << "+ľščťžýáíé";
+    delete loader;
+    */
+    DataLoader *loader = new DataLoader("../semestralnaPraca/dataLoader/rawData/kraje.csv");
+    //UzemnaJednotka<int> *kraj = new UzemnaJednotka<int>();
+    auto kraj = new UzemnaJednotka<int>();
+    /*if (loader->isOpen()) {
+        loader->nextLine();
+        kraj->nastavDalsiParameter(*loader->getNextParameter());
+        kraj->setCode(*loader->getNextParameter());
+        kraj->setOfficialTitle(*loader->getNextParameter());
+        kraj->nastavDalsiParameter(*loader->getNextParameter());
+        kraj->nastavDalsiParameter(*loader->getNextParameter());
+        kraj->nastavDalsiParameter(*loader->getNextParameter());
+        kraj->nastavDalsiParameter(*loader->getNextParameter());
+    }
+    for (int i = 0; i < kraj->getSize(); ++i) {
+        wcout << &kraj[i] << L" ";
+    }
+*/
+    delete kraj;
     delete loader;
     return 0;
 }
