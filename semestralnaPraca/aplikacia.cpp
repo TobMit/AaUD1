c6bc87052c360ef755ce0d7c518685b0c3c0b916
TobMit <80719513+tobmit@users.noreply.github.com>
2022-05-21 12:57:42 +0200

    Pridanie deštruktora

--- semestralnaPraca/aplikacia.cpp
+++ semestralnaPraca/aplikacia.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "aplikacia.h"
+
+Aplikacia::Aplikacia() :
+    kraj(new structures::UnsortedSequenceTable<wstring, StoredData*>),
+    okres(new structures::UnsortedSequenceTable<wstring, StoredData*>),
+    obec(new structures::UnsortedSequenceTable<wstring, StoredData*>),
+    loader(new DataLoader("../semestralnaPraca/dataLoader/rawData/kraje.csv"))
+{
+
+}
+
+Aplikacia::~Aplikacia() {
+    vycistiTable(kraj);
+    delete kraj;
+    kraj = nullptr;
+    vycistiTable(kraj);
+    delete kraj;
+    okres = nullptr;
+    vycistiTable(okres);
+    delete okres;
+    obec = nullptr;
+    vycistiTable(obec);
+    delete obec;
+    obec = nullptr;
+    delete loader;
+    loader = nullptr;
+}
+
+void Aplikacia::vycistiTable(structures::UnsortedSequenceTable<wstring, StoredData *> *table) {
+    for (auto item: *table) {
+        delete item->accessData();
+    }
+}
+
--- semestralnaPraca/aplikacia.h
+++ semestralnaPraca/aplikacia.h
@@ -2,9 +2,28 @@
 // Created by Tobias on 21/05/2022.
 //
 #pragma once
+#include "iostream"
+#include "dataLoader.h"
+#include "uzemnaJednotka.h"
+#include "storedData.h"
+#include "ostatneUdaje.h"
+#include "../structures/table/table.h"
+#include "../structures/table/unsorted_sequence_table.h"
+#include "../structures/table/sorted_sequence_table.h"
 
 class Aplikacia {
+private:
+    structures::UnsortedSequenceTable<wstring, StoredData*> *kraj;
+    structures::UnsortedSequenceTable<wstring, StoredData*> *okres;
+    structures::UnsortedSequenceTable<wstring, StoredData*> *obec;
+    DataLoader *loader;
 
+public:
+    Aplikacia();
+
+    ~Aplikacia();
+private:
+    void vycistiTable(structures::UnsortedSequenceTable<wstring, StoredData*> *table);
 };
 
 
