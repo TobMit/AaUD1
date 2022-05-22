129f77856e77053589136ec4bed42073595414b6
TobMit <80719513+tobmit@users.noreply.github.com>
2022-05-22 19:34:51 +0200

    Vytvorenie

---
+++ semestralnaPraca/dataLoader/tableLoader.h
@@ -0,0 +1,19 @@
+//
+// Created by Tobias on 22/05/2022.
+//
+#pragma once
+class TableLoader
+{
+public:
+    TableLoader();
+
+    virtual ~TableLoader();
+};
+
+TableLoader::TableLoader() {
+
+}
+
+TableLoader::~TableLoader() {
+
+}
