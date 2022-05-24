c64139e06f13883d4afefb32554aea72b334926e
TobMit <80719513+tobmit@users.noreply.github.com>
2022-05-24 20:17:55 +0200

    Výpis všetkých krajov a okresov v poradi.

--- semestralnaPraca/aplikacia.cpp
+++ semestralnaPraca/aplikacia.cpp
@@ -21,22 +21,41 @@ Aplikacia::Aplikacia() :
     tableLoader.indexingTable(*kraj, *okres, *obec, *vzdelanie, *nameIndex,
                           *statIndex, *krajIndex, *okresIndex);
 
-    wstring test;
-    wcout << L"Zadaj nazov: ";
-    std::getline(wcin, test);
-    auto testKraj = nameIndex->find(test);
-    for (int i = 0; i < testKraj->getSize(); ++i) {
-        wcout << testKraj->at(i) << L"\t";
-    }
-    wcout << endl;
-    auto testVzdelanie = vzdelanie->find(testKraj->getCode());
-    auto testVzdelanieNew = dynamic_cast<OstatneUdaje*>(testVzdelanie);
-    if (testVzdelanieNew) {
-        for (int i = 0; i < testVzdelanieNew->getSize(); ++i) {
-            wcout << testVzdelanieNew->intAt(i) << L"\t";
+//    wstring test;
+//    wcout << L"Zadaj nazov: ";
+//    std::getline(wcin, test);
+//    auto testKraj = nameIndex->find(test);
+//    for (int i = 0; i < testKraj->getSize(); ++i) {
+//        wcout << testKraj->at(i) << L"\t";
+//    }
+//    wcout << endl;
+//    auto testVzdelanie = vzdelanie->find(testKraj->getCode());
+//    auto testVzdelanieNew = dynamic_cast<OstatneUdaje*>(testVzdelanie);
+//    if (testVzdelanieNew) {
+//        for (int i = 0; i < testVzdelanieNew->getSize(); ++i) {
+//            wcout << testVzdelanieNew->intAt(i) << L"\t";
+//        }
+//    }
+//    wcout << endl << endl;
+
+    for (auto ixStat: *statIndex) {
+        wcout << ixStat->getKey() << endl;
+        for (auto ixKraj: *ixStat->accessData()) {
+            wcout << "\t";
+            for (int i = 0; i < ixKraj->getSize(); ++i) {
+                wcout << ixKraj->at(i) << L" ";
+            }
+            wcout << endl;
+            auto ixOkres = krajIndex->find(ixKraj->at(5));
+            for (auto item: *ixOkres) {
+                wcout << "\t" << "\t";
+                for (int i = 0; i < item->getSize(); ++i) {
+                    wcout << item->at(i) << L" ";
+                }
+                wcout << endl;
+            }
         }
     }
-    wcout << endl << endl;
 }
 
 Aplikacia::~Aplikacia() {
