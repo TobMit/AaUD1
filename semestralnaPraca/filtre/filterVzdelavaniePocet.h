7c8b15ff8c3f7366418c66b8b2c5b8f98234f31c
TobMit <80719513+tobmit@users.noreply.github.com>
2022-05-27 11:56:31 +0200

    Filter VZpočet hotovy

--- semestralnaPraca/filtre/filterVzdelavaniePocet.h
+++ semestralnaPraca/filtre/filterVzdelavaniePocet.h
@@ -4,14 +4,30 @@
 #pragma once
 
 #include "filter.h"
+#include "criterionVZPocet.h"
 
 class FilterVzPocet : Filter {
-public:
-    bool pass(const StoredData &data) override;
 private:
+    int minimum = 0;
+    int maximum = 0;
+    int index = 0;
+public:
+    FilterVzPocet(int pIndex, int min, int max) :
+        index(pIndex),
+        minimum(min),
+        maximum(max)
+    {
+    };
+    bool pass(const StoredData &data) override;
 
 };
 
 inline bool FilterVzPocet::pass(const StoredData &data) {
-    return false;
+    CriterionVZPocet *criterium = new CriterionVZPocet(index);
+    bool retrunValue = false;
+    if (minimum >= criterium->evaluate(data) && criterium->evaluate(data) <= maximum) {
+        retrunValue = true;
+    }
+    delete criterium;
+    return retrunValue;
 }
