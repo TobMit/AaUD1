465f8591fdd1e9d298b7aa1f850e6eab5f56b75f
TobMit <80719513+tobmit@users.noreply.github.com>
2022-05-27 12:24:54 +0200

    filterVZPodiel.h dokončený.

---
+++ semestralnaPraca/filtre/filterVZPodiel.h
@@ -0,0 +1,26 @@
+//
+// Created by Tobias on 27/05/2022.
+//
+#include "filter.h"
+
+class FilterVzPodiel : Filter<double> {
+private:
+    int index = 0;
+    double minimum = 0;
+    double maximum = 0;
+public:
+    FilterVzPodiel(double min, double max) :
+        minimum(min),
+        maximum(max)
+    {};
+    bool pass(const StoredData &data, Criterion<double> *criterion) override;
+
+};
+
+inline bool FilterVzPodiel::pass(const StoredData &data, Criterion<double> *criterion) {
+    bool retrunValue = false;
+    if (minimum >= criterion->evaluate(data) && criterion->evaluate(data) <= maximum) {
+        retrunValue = true;
+    }
+    return retrunValue;
+}
